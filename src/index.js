ed1355dacbd5650762d29c95307b39b5897e33e8
Mauricio Villegas <maurikiller732@hotmail.com>
2022-09-06 21:43:52 -0400

    componentes creados

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// en index.js
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from './routes/App';
+
+const app = ReactDOM.createRoot(document.getElementById("app"));
+app.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
