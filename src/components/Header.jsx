2f4030ba229269b045d1afb17c5837bdfbdaba88
Mauricio Villegas <maurikiller732@hotmail.com>
2022-09-16 20:30:44 -0400

    react hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
-
+import Menu from '@components/Menu';
+import MyOrder from '../containers/MyOrder'
 import icon_menu from '@icons/icon_menu.svg';
 import logo_yard_sale from '@icons/logo_yard_sale.svg';
 import icon_shopping_cart from '@icons/icon_shopping_cart.svg';
+import AppContext from '../context/AppContext';
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
+    const [toggleOrders, setToggleOrders] = useState(false);
+
+    const { state } = useContext(AppContext);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
     return (
         <nav>
             <img src={icon_menu} alt="menu" className="menu" />
@@ -34,13 +45,23 @@ const Header = () => {
             </div>
             <div className="navbar-right">
                 <ul>
-                    <li className="navbar-email">platzi@example.com</li>
-                    <li className="navbar-shopping-cart">
+                    <li
+                        className="navbar-email"
+                        onClick={handleToggle}
+                    >
+                        platzi@example.com
+                    </li>
+                    <li
+                        className="navbar-shopping-cart"
+                        onClick={() => setToggleOrders(!toggleOrders)}
+                    >
                         <img src={icon_shopping_cart} alt="shopping cart" />
-                        <div>2</div>
+                        {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
                     </li>
                 </ul>
             </div>
+            {toggle && <Menu />}
+            {toggleOrders && <MyOrder />}
         </nav>
     );
 }
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
+import Login from '../pages/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
 import PasswordRecovery from '../pages/PasswordRecovery';
@@ -13,27 +13,32 @@ import CreateAccount from '../pages/CreateAccount';
 import Checkout from '../pages/Checkout';
 import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 import '../styles/global.scss'
 
 const App = () => {
+    const initialState = useInitialState();
     return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/recovery-password" element={<RecoveryPassword />} />
-                    <Route exact path="/password-recovery" element={<PasswordRecovery />} />
-                    <Route exact path="/send-email" element={<SendEmail />} />
-                    <Route exact path="/new-password" element={<NewPassword />} />
-                    <Route exact path="/account" element={<MyAccount />} />
-                    <Route exact path="/signup" element={<CreateAccount />} />
-                    <Route exact path="/checkout" element={<Checkout />} />
-                    <Route exact path="/orders" element={<Orders />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+        <AppContext.Provider value={initialState}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/login" element={<Login />} />
+                        <Route exact path="/recovery-password" element={<RecoveryPassword />} />
+                        <Route exact path="/password-recovery" element={<PasswordRecovery />} />
+                        <Route exact path="/send-email" element={<SendEmail />} />
+                        <Route exact path="/new-password" element={<NewPassword />} />
+                        <Route exact path="/account" element={<MyAccount />} />
+                        <Route exact path="/signup" element={<CreateAccount />} />
+                        <Route exact path="/checkout" element={<Checkout />} />
+                        <Route exact path="/orders" element={<Orders />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </AppContext.Provider>
     );
 }
 
